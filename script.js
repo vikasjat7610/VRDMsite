d049ce9ba443af078de0e9bdf7596065d37739c3
Vikas <130837016+vikasjat7610@users.noreply.github.com>
2024-08-05 10:51:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//bot token
+var telegram_bot_id = "6539268294:AAFBS11KxNL6mnmqm0NVoHY7C0FLf3hz0Yc";
+//chat id
+var chat_id = 1815847082;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nNumber: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
